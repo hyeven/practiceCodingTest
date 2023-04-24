dd10855e4b59ab586d019d241d11774c71238a8a
hyeven <119122459+hyeven@users.noreply.github.com>
2023-04-24 11:18:18 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        double a = scan.nextInt();
+        double b = scan.nextInt();
+        double c = a / b;
+        System.out.println(c);
+    }
+}
